242efe3f15f0922e96bddf56586597760b56ae29
Damodar Bhattarai <damodar.bhattarai.1999@gmail.com>
2022-04-24 13:01:04 +0545

    set width to 20 on excel export

--- app/Exports/ReportExport.php
+++ app/Exports/ReportExport.php
@@ -4,10 +4,14 @@ namespace App\Exports;
 
 use Maatwebsite\Excel\Concerns\FromView;
 use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\RegistersEventListeners;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
 
-
-class ReportExport implements FromView
+class ReportExport implements FromView, WithEvents
 {
+    use RegistersEventListeners;
+
     public $teacherBatchRoutine,$from_date,$to_date,$selectedBatches;
 
     function __construct($teacherBatchRoutine, $from_date, $to_date,$selectedBatches)
@@ -17,6 +21,22 @@ class ReportExport implements FromView
         $this->from_date = $from_date;
         $this->to_date = $to_date;
     }
+
+        /**
+     * @param \Maatwebsite\Excel\Events\AfterSheet $event
+     */
+    public static function afterSheet(AfterSheet $event)
+    {
+        $highestColumn = $event->sheet->getDelegate()->getHighestColumn();
+        $highestColumn++;
+        for ($column = 'A'; $column !== $highestColumn; $column++) {
+            $event->sheet->getDelegate()->getColumnDimension($column)->setWidth(20);
+        }
+    }
+
+
+
+
 
     public function view(): View
     {
